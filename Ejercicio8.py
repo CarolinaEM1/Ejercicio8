4ffa802186fce9d66c37f7dc415f08c2ba507196
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-31 21:42:46 -0600

    primera version

---
+++ Ejercicio8.py
@@ -0,0 +1,17 @@
+#Agregar personajes a una lista
+
+personajes = []
+
+p = {"Nombre":"Aragorn","Clase":"Guerrero","Raza":"Dunadán del Norte"}
+personajes.append(p)
+
+p = {"Nombre":"Gandalf","Clase":"Mago","Raza":"Istar"}
+personajes.append(p)
+
+p = {"Nombre":"Legolas","Clase":"Arquero","Raza":"Elfo Sindar"}
+personajes.append(p)
+
+print(personajes)
+
+#Carolina EM
+
